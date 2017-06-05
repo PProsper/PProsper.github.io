7b246d9e865e40cd8963ccd3299734f1a98f5d31
Patrice Prosper <new>
2017-06-05 10:55:35 -0400

    changes

--- CatMouseGame/release-builds/electron-quick-start-darwin-x64/electron-quick-start.app/Contents/Resources/app/render.js
+++
@@ -1,7 +0,0 @@
-
-  $("#mouse").click(function addScore() { //when mouse clicked value incraments
-    $("#mouse").css("display", "none"),
-        $("#score").html(function() { //mouse disappears on click
-            return score + 1;
-        });
-});
